f091699b57f1bc13632f85aced89fed815ac69e4
Support <142799374+compdir@users.noreply.github.com>
2024-04-08 10:27:18 +0100

    Update Invoke-ListTeamsActivity.ps1

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListTeamsActivity.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListTeamsActivity.ps1
@@ -15,11 +15,17 @@ Function Invoke-ListTeamsActivity {
     # Interact with query parameters or the body of the request.
     $TenantFilter = $Request.Query.TenantFilter
     $type = $request.query.Type
-    $GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/get$($type)Detail(period='D30')" -tenantid $TenantFilter | ConvertFrom-Csv | Select-Object @{ Name = 'UPN'; Expression = { $_.'User Principal Name' } },
+    $GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/get$($type)Detail(period='D7')" -tenantid $TenantFilter | ConvertFrom-Csv | Select-Object @{ Name = 'UPN'; Expression = { $_.'User Principal Name' } },
     @{ Name = 'LastActive'; Expression = { $_.'Last Activity Date' } },
     @{ Name = 'TeamsChat'; Expression = { $_.'Team Chat Message Count' } },
     @{ Name = 'CallCount'; Expression = { $_.'Call Count' } },
-    @{ Name = 'MeetingCount'; Expression = { $_.'Meeting Count' } }
+    @{ Name = 'MeetingCount'; Expression = { $_.'Meeting Count' } },
+    @{ Name = 'AudioCallDuration'; Expression = { $_.'Audio Duration' } },
+    @{ Name = 'VideoCallDuration'; Expression = { $_.'Video Duration' } },
+    @{ Name = 'ScreenShareDuration'; Expression = { $_.'Screen Share Duration' } },
+    @{ Name = 'PrivateChat'; Expression = { $_.'Private Chat Message Count' } }
+                         
+                        
 
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
